6590d1d2b8affef59668179d7ac037877db7f2af
Luis Maldonado <learayamaldonado@gmail.com>
2023-06-22 17:07:45 -0400

    Primer Commit

---
+++ src/blackjack/usercases/crear-deck.js
@@ -0,0 +1,32 @@
+import _ from 'underscore';
+/**
+ * Esta función crea un nuevo deck
+ * @param {Array<String>} tiposDeCarta ejemplo: ['C', 'D', 'H', 'S']
+ * @param {Array<String>} tiposEspeciales ejemplo: ['A', 'J', 'Q', 'K']
+ * @returns {array<String>}; retorna un nuevo deck de cartas
+ */
+export const crearDeck = (tiposDeCarta, tiposEspeciales) => {
+
+  if (!tiposDeCarta || tiposDeCarta.length === 0)
+    throw new Error('TiposDeCarta es obligatorio como un arreglo de string');
+  if (!tiposEspeciales || tiposEspeciales.length === 0)
+    throw new Error('TiposDeCarta es obligatorio como un arreglo de string');
+
+  let deck = [];
+
+  for (let i = 2; i <= 10; i++) {
+    for (let tipo of tiposDeCarta) {
+      deck.push(i + tipo);
+    }
+  }
+
+  for (let tipo of tiposDeCarta) {
+    for (let esp of tiposEspeciales) {
+      deck.push(esp + tipo);
+    }
+  }
+  // console.log( deck );
+  deck = _.shuffle(deck);
+  // console.log(deck);
+  return deck;
+}
